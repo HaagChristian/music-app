c5fd19e42179d9f808c9ec532e8e1327f8b545b9
selinaweh <selinaweh11@gmail.com>
2024-02-24 15:28:48 +0100

    added mapping functions

--- src/service/mapping/map_db_data.py
+++ src/service/mapping/map_db_data.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from src.api.myapi.metadata_model import MetadataFromSearch, Artist, MetadataToChange, MetadataId3Input
+from src.api.myapi.music_db_models import FileDetailModel, SongDetailModel
 from src.database.musicDB.db_models import Song
 
 
@@ -42,3 +43,69 @@ def input_mapping_from_change_metadata(metadata_to_change: MetadataToChange) -> 
                                     title=metadata_to_change.title, artists=artist_str)
 
     return data_for_id3
+
+
+def file_obj_to_model(file_obj):
+    """converts SQLAlchemy file object to dictionary for the pydantic model."""
+    file_detail_data = {
+        "file_id": file_obj.FILE_ID,
+        "file_type": file_obj.FILE_TYPE,
+        "file_name": file_obj.FILE_NAME,
+        "song": {
+            "song_id": file_obj.song.SONG_ID,
+            "song_title": file_obj.song.TITLE,
+            "duration": file_obj.song.DURATION,
+            "file_path": file_obj.song.file.FILE_NAME,
+            "bit_rate": file_obj.song.BIT_RATE,
+            "sample_rate": file_obj.song.SAMPLE_RATE,
+            "release_date": file_obj.song.RELEASE_DATE.strftime("%Y-%m-%d") if file_obj.song.RELEASE_DATE else None,
+            "album": {
+                "album_id": file_obj.song.album.ALBUM_ID,
+                "album_name": file_obj.song.album.ALBUM_NAME,
+                "artist_id": file_obj.song.album.artist_id,
+            },
+            "genre": {
+                "genre_id": file_obj.song.genre.GENRE_ID,
+                "genre_name": file_obj.song.genre.GENRE_NAME,
+            },
+            "artists": [
+                {"artist_id": artist.ARTIST_ID, "artist_name": artist.ARTIST_NAME} for artist in file_obj.song.artist
+            ]
+        }
+    }
+    return FileDetailModel(**file_detail_data)
+
+
+
+def song_obj_to_model(song_obj):
+    """converts SQLAlchemy song object to dictionary for the pydantic model."""
+    artists_list = [{"artist_id": artist.artist_id, "artist_name": artist.artist_name} for artist in song_obj.artists]
+
+    file_model = {
+        "file_id": song_obj.file.FILE_ID,
+        "file_type": song_obj.file.FILE_TYPE,
+        "file_name": song_obj.file.FILE_NAME,
+    }
+
+    song_detail_data = {
+        "song_id": song_obj.SONG_ID,
+        "song_title": song_obj.TITLE,
+        "duration": song_obj.DURATION,
+        "file_path": song_obj.file.FILE_NAME,
+        "bit_rate": song_obj.BIT_RATE,
+        "sample_rate": song_obj.SAMPLE_RATE,
+        "release_date": song_obj.RELEASE_DATE.strftime("%Y-%m-%d") if song_obj.RELEASE_DATE else None,
+        "album": {
+            "album_id": song_obj.album.ALBUM_ID,
+            "album_name": song_obj.album.ALBUM_NAME,
+            "artist_id": song_obj.album.artist_id,
+        },
+        "genre": {
+            "genre_id": song_obj.genre.GENRE_ID,
+            "genre_name": song_obj.genre.GENRE_NAME,
+        },
+        "artists": artists_list,
+        "file": file_model,
+    }
+
+    return SongDetailModel(**song_detail_data)

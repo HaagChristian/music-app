70e1c0b9ea4fbf1ffeee4ef740d0b49b01f9ef03
selinaweh <selinaweh11@gmail.com>
2024-02-25 13:29:22 +0100

    adapted cruds

--- src/api/routers/crud.py
+++ src/api/routers/crud.py
@@ -3,10 +3,10 @@ from fastapi.security import HTTPBearer
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import NoResultFound
 from src.database.musicDB.db import get_db_music
-from src.api.myapi.music_db_models import FileDetailModel, Song, File, SongWithRelationsAndFile
-from src.database.musicDB.db_crud import get_file_and_song_by_id, get_song_by_id, get_file_by_id, get_file_by_song_id, \
+from src.api.myapi.music_db_models import Song, File, SongWithRelationsAndFile
+from src.database.musicDB.db_crud import get_song_by_id, get_file_by_id, get_file_by_song_id, \
     get_song_and_file_by_song_id
-from src.service.mapping.map_db_data import file_obj_to_model, song_and_file_obj_to_model
+from src.service.mapping.map_db_data import song_and_file_obj_to_model
 from src.settings.error_messages import NO_DB_RESULT_FOUND
 
 
@@ -41,15 +41,6 @@ def get_song(song_id: int, db: Session = Depends(get_db_music)):
         raise NoResultFound(NO_DB_RESULT_FOUND)
     return song
 
-
-"""returns song with file and relations accessed by file_id"""
-@router.get("/file_and_song/{file_id}", response_model=FileDetailModel)
-def get_file_and_song(file_id: int, db: Session = Depends(get_db_music)):
-    file_obj = get_file_and_song_by_id(db, file_id)
-    if not file_obj:
-        raise NoResultFound(NO_DB_RESULT_FOUND)
-
-    return file_obj_to_model(file_obj)
 
 
 """returns song with file and relations accessed by song_id"""

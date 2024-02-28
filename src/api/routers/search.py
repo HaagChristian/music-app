cbd109b598db9bf54d2155b1d147de6b06f8a959
selinaweh <selinaweh11@gmail.com>
2024-02-28 08:54:21 +0100

    adapted search

--- src/api/routers/search.py
+++ src/api/routers/search.py
@@ -19,45 +19,6 @@ router = APIRouter(
 )
 
 
-"""endpoints for songs with relations"""
-'''
-@router.get("/search/song_by_title", response_model=List[SongWithRelations])
-def search_song_by_title(title: str, db: Session = Depends(get_db_music)):
-    songs = search_song_by_title(db, title)
-    songs_with_rel = [song_obj_to_model(song) for song in songs]
-    return songs_with_rel
-
-
-@router.get("/search/song_by_genre", response_model=List[SongWithRelations])
-def search_song_by_genre(genre_name: str, db: Session = Depends(get_db_music)):
-    songs = search_songs_by_genre(db, genre_name)
-    songs_with_rel = [song_obj_to_model(song) for song in songs]
-    return songs_with_rel
-
-
-@router.get("/search/song_by_artist", response_model=List[SongWithRelations])
-def search_song_by_artist(artist_name: str, db: Session = Depends(get_db_music)):
-    songs = search_songs_by_artist(db, artist_name)
-    songs_with_rel = [song_obj_to_model(song) for song in songs]
-    return songs_with_rel
-
-
-@router.get("/search/song_by_album", response_model=List[SongWithRelations])
-def search_song_by_album(album_name: str, db: Session = Depends(get_db_music)):
-    songs = search_songs_by_album(db, album_name)
-    songs_with_rel = [song_obj_to_model(song) for song in songs]
-    return songs_with_rel
-
-
-@router.get("/search/combined", response_model=List[SongWithRelations])
-def search_combined(db: Session = Depends(get_db_music), title: str = None, genre_name: str = None,
-                    artist_name: str = None, album_name: str = None):
-    songs = search_songs_combined(db, title, genre_name, artist_name, album_name)
-    songs_with_rel: List[SongWithRelations] = [song_obj_to_model(song) for song in songs]
-    return songs_with_rel
-'''
-
-
 @router.get("/search/all_criteria", response_model=Dict[str, List[str]])
 def get_all_search_criteria(db: Session = Depends(get_db_music)):
     criteria_dict = fetch_all_search_criteria(db)
@@ -65,7 +26,7 @@ def get_all_search_criteria(db: Session = Depends(get_db_music)):
         raise NoResultFound(DB_NO_RESULT_FOUND)
     return criteria_dict
 
-# TODO: fix input validator
+
 @router.post("/search/combined", response_model=List[SongWithRelations])
 def search_combined(criteria: SearchCriteria, db: Session = Depends(get_db_music)):
     songs = search_songs_combined(db, criteria.title, criteria.genre_name, criteria.artist_name, criteria.album_name)
--- src/database/musicDB/db_search.py
+++ src/database/musicDB/db_search.py
@@ -4,64 +4,31 @@ from sqlalchemy.orm import Session, joinedload
 from src.database.musicDB.db_models import Song, Artist, Album, Genre
 
 
-"""Search for songs in the database based on various criteria."""
-# combined search returns song objects with joined artist, album and genre
-'''
-def search_song_by_title(db: Session, title: str):
-    return db.query(Song).filter(Song.TITLE.like(f"%{title}%")) \
-        .options(
-            joinedload(Song.album),
-            joinedload(Song.genre),
-            joinedload(Song.artists)
-        ).all()
-
-def search_songs_by_genre(db: Session, genre_name: str):
-    return db.query(Song).join(Genre).filter(Genre.GENRE_NAME.like(f'%{genre_name}%')) \
-        .options(
-            joinedload(Song.album),
-            joinedload(Song.artists)
-        ).all()
-
-def search_songs_by_artist(db: Session, artist_name: str):
-    return db.query(Song).join(Song.artists).join(Artist).filter(Artist.ARTIST_NAME.like(f'%{artist_name}%')) \
-        .options(
-            joinedload(Song.album),
-            joinedload(Song.genre)
-        ).all()
-
-def search_songs_by_album(db: Session, album_name: str):
-    return db.query(Song).join(Album).filter(Album.ALBUM_NAME.like(f'%{album_name}%')) \
-        .options(
-            joinedload(Song.genre),
-            joinedload(Song.artists)
-        ).all()
-'''
-
-
 def fetch_all_search_criteria(db: Session):
     criteria_dict = {}
 
     titles = db.query(Song.TITLE).distinct().all()
-    titles_list = [title[0] for title in titles]
+    titles_list = [item for title in titles for item in title[0].split("/")]
     if titles_list:
         criteria_dict["title"] = titles_list
 
     artists = db.query(Artist.ARTIST_NAME).distinct().all()
-    artists_list = [artist[0] for artist in artists]
+    artists_list = [item for artist in artists for item in artist[0].split("/")]
     if artists_list:
         criteria_dict["interpret"] = artists_list
 
     albums = db.query(Album.ALBUM_NAME).distinct().all()
-    albums_list = [album[0] for album in albums]
+    albums_list = [item for album in albums for item in album[0].split("/")]
     if albums_list:
         criteria_dict["album"] = albums_list
 
     genres = db.query(Genre.GENRE_NAME).distinct().all()
-    genres_list = [genre[0] for genre in genres]
+    genres_list = [item for genre in genres for item in genre[0].split("/")]
     if genres_list:
         criteria_dict["genre"] = genres_list
 
     return criteria_dict
+
 
 def search_songs_combined(db: Session, title: str = None, genre_name: str = None, artist_name: str = None,
                           album_name: str = None):
@@ -83,9 +50,12 @@ def search_songs_combined(db: Session, title: str = None, genre_name: str = None
         ).all()
 
 
-"""Combined search for title and artist."""
+
 
 def search_for_title_and_artist(db: Session, title: str, artist: str):
+    """
+    Combined search for title and artist.
+    """
     if title and artist:
         return db.query(Song).join(Song.artist).join(Song.album).filter(
             and_(

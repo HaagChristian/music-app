03e24d7644eea966b29bd9ae223a3200e7c4ebcb
selinaweh <selinaweh11@gmail.com>
2024-02-29 16:12:02 +0100

    temp file operations file

---
+++ src/api/middleware/temp_file_operations.py
@@ -0,0 +1,24 @@
+import tempfile
+from tempfile import TemporaryFile
+import os
+
+from starlette.background import BackgroundTask
+from starlette.responses import FileResponse
+
+
+def create_and_return_file(file):
+    """
+    Create a temporary file and return it as a FileResponse.
+    """
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file_path = temp_file.name
+        temp_file.write(file.FILE_DATA)
+        return FileResponse(temp_file_path, background=BackgroundTask(cleanup, temp_file_path=temp_file_path, temp_file=temp_file))
+
+
+def cleanup(temp_file: TemporaryFile, temp_file_path: str):
+    """
+    Delete tempfile after response is sent.
+    """
+    temp_file.close()
+    os.remove(temp_file_path)  # delete the temporary file --> necessary because of the delete=False parameter

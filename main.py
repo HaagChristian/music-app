115770b7811e679dae63fa122dcf7a1964b8d885
christian <Christian@weinverkauf-haag.de>
2024-03-03 18:05:48 +0100

    added cors route

--- main.py
+++ main.py
@@ -49,7 +49,8 @@ origins = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
-    "http://localhost:4200"
+    "http://localhost:4200",
+    "http://127.0.0.1"
 ]
 
 app.add_middleware(
